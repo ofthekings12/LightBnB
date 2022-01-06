a8044285805eeb9db339e07126cde8d98e42c583
Mikko De Los Reyes <mikkode212@gmail.com>
2022-01-06 04:08:13 +0000

    this commit adds select queries

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -17,7 +17,7 @@ VALUES (3, 3, '2018-09-11', '2018-09-26'),
 (4, 1, '2021-10-01', '2021-10-14'),
 (1, 4, '2014-10-21', '2014-10-21');
 
-INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
+INSERT INTO property_reviews(guest_id, property_id, reservation_id, rating, message)
 VALUES 
 (2, 2, 2, 3, 'message'),
 (1, 4, 4, 4, 'message'),
